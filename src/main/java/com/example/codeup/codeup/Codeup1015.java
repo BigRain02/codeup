a904474d3d4c04c90c6143cd21c36ccb356e9662
zxfg223 <zxfg223@gmail.com>
2023-04-20 15:58:57 +0900

    Code Up : Test1015 - 1015번째 문제 풀이

--- src/main/java/com/example/codeup/codeup/Codeup1015.java
+++ src/main/java/com/example/codeup/codeup/Codeup1015.java
@@ -2,11 +2,10 @@ package com.example.codeup.codeup;
 
 import java.util.Scanner;
 
-public class Codeup1014 {
+public class Codeup1015 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String iVal1 = sc.next();
-        String iVal2 = sc.next();
-        System.out.printf("%d %d",iVal2,iVal1);
+        System.out.printf("%.2f",sc.nextFloat());
+
     }
 }
