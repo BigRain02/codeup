805d0dff5b366701f6ae4c2581d577bf6e26897e
zxfg223 <zxfg223@gmail.com>
2023-04-20 10:33:52 +0900

    Code Up : Test1010~1012 - 1010~1012번째 문제 풀이

---
+++ src/main/java/com/example/codeup/codeup/Codeup1010.java
@@ -0,0 +1,9 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup1011.java
@@ -0,0 +1,10 @@
+package com.example.codeup.codeup;
+import java.util.Scanner;
+public class Codeup1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println(x);
+
+    }
+}
