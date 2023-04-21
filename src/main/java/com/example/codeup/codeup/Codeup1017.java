23202611ccfcd462553f8f7f6b86fb6a82112f23
zxfg223 <zxfg223@gmail.com>
2023-04-21 10:42:29 +0900

    Code Up : Test1017 - 1017번째 문제 풀이

---
+++ src/main/java/com/example/codeup/codeup/Codeup1017.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int scanN = sc.nextInt();
+        System.out.printf("%d %d %d",scanN, scanN, scanN);
+
+    }
+}
