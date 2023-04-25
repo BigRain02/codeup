2f9f4cd6b910d698cf069980050e751341d4a4c1
zxfg223 <zxfg223@gmail.com>
2023-04-25 09:21:29 +0900

    Code Up : Test1031~1037 -1031~1037 문제풀이 코드

---
+++ src/main/java/com/example/codeup/codeup/Codeup1031.java
@@ -0,0 +1,10 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%s",Integer.toOctalString(sc.nextInt()));
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup1037.java
@@ -0,0 +1,16 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+
+public class Codeup1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num >= 0 && num <= 255) {
+            System.out.printf("%c", num);
+        }
+    }
+}
+
+
