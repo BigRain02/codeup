aafa0e1922f4fba029015fd9507fb8e0039e3158
zxfg223 <zxfg223@gmail.com>
2023-04-27 15:09:14 +0900

    Code Up : Test1059~1062까지 풀이 - 1062번까지

---
+++ src/main/java/com/example/codeup/codeup/Codeup105x/Codeup1059.java
@@ -0,0 +1,11 @@
+package com.example.codeup.codeup.Codeup105x;
+
+import java.util.Scanner;
+
+public class Codeup1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(~sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup106x/Codeup1060.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup.Codeup106x;
+
+
+import java.util.Scanner;
+
+public class Codeup1060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() & sc.nextInt());
+    }
+}
