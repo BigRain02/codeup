7499fd90ca4e9207bb7520c6c4bf2fcb004efef5
zxfg223 <zxfg223@gmail.com>
2023-04-28 14:17:57 +0900

    Code Up : Test1116~1121번 - 1116~1121번 문제 풀이

---
+++ src/main/java/com/example/codeup/codeup/Codeup111x/Codeup1119.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup.Codeup111x;
+
+import java.util.Scanner;
+
+public class Codeup1119 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+
+        System.out.printf("%d",iVal*24);
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup112x/Codeup1121.java
@@ -0,0 +1,14 @@
+package com.example.codeup.codeup.Codeup112x;
+
+import java.util.Scanner;
+
+public class Codeup1121 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal1 = sc.nextInt();
+        int iVal2 = sc.nextInt();
+
+        System.out.printf("%d",iVal1%iVal2);
+
+    }
+}
