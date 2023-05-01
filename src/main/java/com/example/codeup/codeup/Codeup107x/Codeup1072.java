db6e15c6a97466125fc53ca65ec6b40f9d4a24b7
zxfg223 <zxfg223@gmail.com>
2023-05-01 10:49:40 +0900

    Code Up : Test1071~1072

---
+++ src/main/java/com/example/codeup/codeup/Codeup107x/Codeup1072.java
@@ -0,0 +1,14 @@
+package com.example.codeup.codeup.Codeup107x;
+
+import java.util.Scanner;
+
+public class Codeup1072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0; i < n; i++){
+            int iVal = sc.nextInt();
+            System.out.println(iVal);
+        }
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup110x/Codeup1107.java
@@ -0,0 +1,5 @@
+package com.example.codeup.codeup.Codeup110x;
+
+public class Codeup1107
+{
+}
