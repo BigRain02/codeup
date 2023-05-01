aaa0d184a9d8496758ee8d21cd3cbc96656eae47
zxfg223 <zxfg223@gmail.com>
2023-05-01 09:07:11 +0900

    Code Up : Test1103,1106

---
+++ src/main/java/com/example/codeup/codeup/Codeup110x/Codeup1106.java
@@ -0,0 +1,9 @@
+package com.example.codeup.codeup.Codeup110x;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        int min_a = 0x80000000;
+        int max_b = 0x7fffffff;
+        System.out.printf("%d %d",min_a,max_b);
+    }
+}
