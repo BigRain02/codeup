3de1ac535bd60232b29e613874054744099351e0
zxfg223 <zxfg223@gmail.com>
2023-05-03 10:18:05 +0900

    Code Up : Test1071,1073 do-while - do while문으로 수정

--- src/main/java/com/example/codeup/codeup/Codeup107x/Codeup1073.java
+++ src/main/java/com/example/codeup/codeup/Codeup107x/Codeup1073.java
@@ -5,16 +5,11 @@ import java.util.Scanner;
 public class Codeup1073 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = 1; // 임의의 값으로 초기화
-        while (num != 0) {
-            num = sc.nextInt();
-            switch (num) {
-                case 0:
-                    break;
-                default:
-                    System.out.println(num);
-                    break;
-            }
+        int number;
+        do {
+            number = sc.nextInt();
+            if(number!=0)System.out.println(number);
         }
+        while (number != 0);
     }
 }
